0a4584737e64b3e5c2e480a9bb8d3a6f5d4c479e
ktowner15 <33582642+ktowner15@users.noreply.github.com>
2020-07-23 09:39:00 -0500

    Got table retrieval working within the redirect page

--- src/Components/Click.jsx
+++ src/Components/Click.jsx
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
 import "./Click.css";
+import e_table from "../Resources/env_table";
 
 class Click extends Component {
-  getUsage() {}
+  x() {
+    return "Hello";
+  }
+
+  getClickTable() {
+    console.log(e_table.apps[0]);
+    return e_table.apps[0];
+  }
 
   render() {
     return (
       <div>
-        {/* infoPage is replaced by this */}
-        <button onClick={getUsage()}>Click me for alert</button>
+        <div>This is a heading</div>
+        <div>Welcome</div>
+        {/* THIS WORKS */}
+        {/* {console.log(e_table.apps[0])} */}
+
+        {/* THIS WORKS */}
+        {/* {e_table.apps[0].list[0].name} */}
+
+        {/* THIS WORKS */}
+        {console.log(e_table.apps[0].appName)}
+        {e_table.apps[0].list.forEach((element) => {
+          console.log(element.name);
+        })}
       </div>
     );
   }
---
+++ src/Resources/env_table.js
@@ -0,0 +1,29 @@
+export default {
+  apps: [
+    // Click environments are first, since Click has id of 0
+    {
+      appName: "Click",
+      list: [
+        { name: "AmerenD1", usage: "P3" },
+        { name: "AmerenD3", usage: "No one" },
+        { name: "AmerenD4", usage: "P2 Build and P1 Dev" },
+        { name: "AmerenD5", usage: "P2 SIT" },
+        { name: "AmerenD6", usage: "P1 Team" },
+        { name: "AmerenD7", usage: "P2 UAT" },
+        { name: "AmerenD8", usage: "P3 POC" },
+        { name: "AmerenD9", usage: "P3 ADMS, CSS Build and Integration Test" },
+        { name: "AmerenD10", usage: "P2 Cut-over" },
+        { name: "Ameren11", usage: "P3 Storm Testing" },
+        { name: "AmerenSB", usage: "P3 Build" }
+      ]
+    },
+    //Whatever environment is next will have an id corresponding to the position at which its environments are placed within the 'apps' array
+    {
+      appName: "EnvNext",
+      list: [
+        { name: "next1", usage: "usage1" },
+        { name: "next2", usage: "usage2" }
+      ]
+    }
+  ]
+};

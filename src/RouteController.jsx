189382596d8903c6cfda59d617f7e262ca92a2e3
ktowner15 <33582642+ktowner15@users.noreply.github.com>
2020-07-27 11:33:51 -0500

    Played around with JQuery and redirecting in different ways see RouteController. Trying to get the URL to change to reflect the environment name (called it ':title').  Problem is we can't pass it to the buttonClicked handler without using either props or parameters, both of which break in various ways. Param.s cause it to run on page load

--- src/RouteController.jsx
+++ src/RouteController.jsx
@@ -20,22 +20,35 @@ class RouteController extends Component {
 
             {/* TODO: find a way to render additional tiles on the page, and have them redirect an env page that depends on the props specified in the 'entry' object */}
             {/* FIXME: this works if you have  */}
-            <Route path="/" exact component={Entry} />
+
+            {/* FIXME: left off here */}
+            <div id="Click">
+              <Route ref="Click" path="/" exact component={Entry} />
+            </div>
+
+            {/* <Route
+              path="/"
+              exact
+              component={() => <Entry title="env2" />}
+            /> */}
+
             {/* but it doesn't work if you change it to include multiple Entries. */}
             {/* <Route path="/" exact component={()=><div>
               <Entry/>
               <Entry/>
             </div>} /> */}
 
-            {/* The following will be routes for each new environment that we add.  'id' refers to its position within the JS array. */}
-            <Route
-              path="/click"
-              component={() => <Click title="Click" id="0" />}
-            />
-            <Route
+            {/* The following will be routes for each new environment that we add. */}
+            {/* 'id' refers to its position within the JS array. */}
+            {/* <Route
+              path="/:title"
+              component={() => <Click title="click" id="0" />}
+            /> */}
+            {/* <Route
               path="/env2"
-              component={() => <Click title="Env2" id="1" />}
-            />
+              component={() => <Click title="cnv2" id="1" />}
+            /> */}
+            <Route path="/:title" component={Click} />
             {/* Error component triggers if none of the above routes can be loaded successfully */}
             <Route component={ErrorComponent} />
           </Switch>
